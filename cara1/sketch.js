45ff598964e7200081d0375e5c520328d7fccf98
ichanchi-dotcom <ichanchi@iespompeufabra.cat>
2026-09-23 14:17:45 +0200

    Create sketch.js

---
+++ cara1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(242,236,218);
+  ellipse(300,300,200,250);
+  ellipse(250,250,50,30);
+  ellipse(350,250,50,30);
+arc(300,350,99,40,0, PI);
+  triangle(270,300,330,240,310,280)
+}
